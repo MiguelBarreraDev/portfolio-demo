bfbb432c113e977ee34cec0f98cf0d0a5d8e6583
MiguelBarreraDev <barreram987@gmail.com>
2022-09-22 00:07:38 -0500

    Fix: Correct value for href property

--- src/components/Home/SocialNetworks.jsx
+++ src/components/Home/SocialNetworks.jsx
@@ -6,22 +6,20 @@ export default function SocialNetworks () {
   return (
     <div className="social-networks">
       <a
-        href={socialNetworksLinks.GITHUB}
+        href={socialNetworksLinks.GITHUB.to}
         target="_blank"
-        className="link-github"
       >
         <GitHubIcon />
       </a>
       <a
-        href={socialNetworksLinks.INSTAGRAM}
-        className="link-instagram"
+        href={socialNetworksLinks.INSTAGRAM.to}
+        target="_blank"
       >
         <InstagramIcon />
       </a>
       <a
-        href={socialNetworksLinks.LINKEDIN}
+        href={socialNetworksLinks.LINKEDIN.to}
         target="_blank"
-        className="link-linkedin"
       >
         <LinkedinIcon />
       </a>
