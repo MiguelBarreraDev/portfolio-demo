5da83391a603cf91eadd73ed71e480d25c54f11a
MiguelBarreraDev <barreram987@gmail.com>
2022-09-22 00:01:10 -0500

    Add: Icon of he page

--- src/components/Header/SocialNetworks.jsx
+++ src/components/Header/SocialNetworks.jsx
@@ -6,20 +6,20 @@ export default function SocialNetworks () {
   return (
     <div className="social-networks">
       <a
-        href={socialNetworksLinks.GITHUB}
+        href={socialNetworksLinks.GITHUB.to}
         target="_blank"
         className="link-github"
       >
         <GitHubIcon />
       </a>
       <a
-        href={socialNetworksLinks.INSTAGRAM}
+        href={socialNetworksLinks.INSTAGRAM.to}
         className="link-instagram"
       >
         <InstagramIcon />
       </a>
       <a
-        href={socialNetworksLinks.LINKEDIN}
+        href={socialNetworksLinks.LINKEDIN.to}
         target="_blank"
         className="link-linkedin"
       >
