be4ec74247a2120ab324260999721336f270bfbf
MiguelBarreraDev <barreram987@gmail.com>
2022-09-22 01:48:41 -0500

    Add: Funcionality to the button

--- src/components/About/Biography/Button.jsx
+++ src/components/About/Biography/Button.jsx
@@ -1,10 +1,11 @@
 import { ToBottomIcon } from '@/assets/images/icons'
 import './styles/Button.css'
+import CVDocument from '@/assets/documents/CVMiguelBarreraPalomares.pdf'
 
 export default function ButtonViewCV () {
   return (
     <div className="btn_view-cv">
-      <a href="#">
+      <a href='#' onClick={() => open(CVDocument)} target='_blank'>
         <span>View CV</span>
         <span>
           <ToBottomIcon />
